d2aa7bdbe92bc584f64eed3d6a48de5f923fa740
David Li <taweili@yahoo.com>
2012-02-07 10:13:03 +0800

    try new fonts

--- src/main/java/edu/mit/blocks/renderable/BlockLabel.java
+++ src/main/java/edu/mit/blocks/renderable/BlockLabel.java
@@ -40,12 +40,12 @@ public class BlockLabel implements MouseListener, MouseMotionListener, KeyListen
 
         NAME_LABEL, PAGE_LABEL, PORT_LABEL, DATA_LABEL
     }
-    public final static Font blockFontSmall_Bold = new Font("Arial", Font.BOLD, 7);
-    public final static Font blockFontMedium_Bold = new Font("Arial", Font.BOLD, 10);
-    public final static Font blockFontLarge_Bold = new Font("Arial", Font.BOLD, 12);
-    public final static Font blockFontSmall_Plain = new Font("Arial", Font.PLAIN, 7);
-    public final static Font blockFontMedium_Plain = new Font("Arial", Font.PLAIN, 10);
-    public final static Font blockFontLarge_Plain = new Font("Arial", Font.PLAIN, 12);
+    public final static Font blockFontSmall_Bold = new Font("Monospaced", Font.BOLD, 7);
+    public final static Font blockFontMedium_Bold = new Font("Monospaced", Font.BOLD, 10);
+    public final static Font blockFontLarge_Bold = new Font("Monospaced", Font.BOLD, 12);
+    public final static Font blockFontSmall_Plain = new Font("Monospaced", Font.PLAIN, 7);
+    public final static Font blockFontMedium_Plain = new Font("Monospaced", Font.PLAIN, 10);
+    public final static Font blockFontLarge_Plain = new Font("Monospaced", Font.PLAIN, 12);
     private LabelWidget widget;
     /** These keys inputs are delegated back to renderable block */
     private final char[] validOperators = {'-', '+', '/', '*', '=', '<', '>', 'x', 'X'};
