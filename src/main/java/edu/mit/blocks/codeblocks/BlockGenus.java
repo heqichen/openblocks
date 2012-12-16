04e55e47c32195c5ca3d484ee72649c07c62443a
Leonilde Diogenes Gurgel Neto <ldgneto@gmail.com>
2012-12-16 14:14:39 -0200

    Update src/main/java/edu/mit/blocks/codeblocks/BlockGenus.java
    In loadBlockGenera method
    Added attribName to send it to loadGenusDescription method
    to translate the ToolTip following the ardublock.properties 

--- src/main/java/edu/mit/blocks/codeblocks/BlockGenus.java
+++ src/main/java/edu/mit/blocks/codeblocks/BlockGenus.java
@@ -465,19 +465,28 @@ public class BlockGenus {
         eGroup.add(new BlockConnector(socket));
     }
 
+    /**
+     * Modified to translate the ToolTip
+	 *  ldgneto@gmail.com 
+	 * */
     private static void loadGenusDescription(NodeList descriptions, BlockGenus genus) {
-        Node description;
-        for (int k = 0; k < descriptions.getLength(); k++) {
-            description = descriptions.item(k);
-            if (description.getNodeName().equals("text")) {
-                genus.blockDescription = description.getTextContent();
-            } else if (description.getNodeName().equals("arg-description")) {
-                String argumentDescription = description.getTextContent();
-                if (argumentDescription != null) {
-                    genus.argumentDescriptions.add(argumentDescription);
-                }
-            }
-        }
+        Node description;        
+		ResourceBundle bundle = ResourceBundle.getBundle("com/ardublock/block/ardublock");	
+		for (int k = 0; k < descriptions.getLength(); k++) {
+			description = descriptions.item(k);	
+			if (description.getNodeName().equals("text") && !attribName.equals(null)) {
+				try{//Trying to bundle the attribute
+					genus.blockDescription = bundle.getString("tip." + attribName);
+				}catch (MissingResourceException e) {
+					genus.blockDescription = description.getTextContent();
+				}
+			} else if (description.getNodeName().equals("arg-description")) {
+				String argumentDescription = description.getTextContent();
+				if (argumentDescription != null) {
+					genus.argumentDescriptions.add(argumentDescription);
+				}
+			}
+		}
     }
 
     /**
@@ -771,6 +780,7 @@ public class BlockGenus {
         NodeList genusNodes = root.getElementsByTagName("BlockGenus"); //look for genus
         Node genusNode;
         StringTokenizer col;
+        String attribName = "";//Added to get attribute name
         for (int i = 0; i < genusNodes.getLength(); i++) { //find them
             genusNode = genusNodes.item(i);
             if (genusNode.getNodeName().equals("BlockGenus")) {
@@ -778,6 +788,8 @@ public class BlockGenus {
                 BlockGenus newGenus = new BlockGenus(env);
                 //first, parse out the attributes
                 nameMatcher = attrExtractor.matcher(genusNode.getAttributes().getNamedItem("name").toString());
+                //Attribute name to loadGenusDescription method
+                attribName = genusNode.getAttributes().getNamedItem("name").getNodeValue();
                 if (nameMatcher.find()) {
                     newGenus.genusName = nameMatcher.group(1);
                 }
@@ -867,7 +879,7 @@ public class BlockGenus {
                     genusChild = genusChildren.item(j);
                     if (genusChild.getNodeName().equals("description")) {
                         /// LOAD BLOCK GENUS DESCRIPTION ///
-                        loadGenusDescription(genusChild.getChildNodes(), newGenus);
+                        loadGenusDescription(genusChild.getChildNodes(), newGenus, attribName);
                     } else if (genusChild.getNodeName().equals("BlockConnectors")) {
                         /// LOAD BLOCK CONNECTOR INFORMATION ///
                         loadBlockConnectorInformation(workspace, genusChild.getChildNodes(), newGenus);
