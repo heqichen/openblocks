085791b3adfad6be22d9abe02c41217e7ab99082
David Li <taweili@yahoo.com>
2013-08-18 09:26:01 +0800

    Light gray drop shadow.

--- src/main/java/edu/mit/blocks/renderable/BlockLabel.java
+++ src/main/java/edu/mit/blocks/renderable/BlockLabel.java
@@ -39,12 +39,12 @@ public class BlockLabel implements MouseListener, MouseMotionListener, KeyListen
     public enum Type {
         NAME_LABEL, PAGE_LABEL, PORT_LABEL, DATA_LABEL
     }
-    public final static Font blockFontSmall_Bold = new Font("Monospaced", Font.BOLD, 7);
-    public final static Font blockFontMedium_Bold = new Font("Monospaced", Font.BOLD, 10);
-    public final static Font blockFontLarge_Bold = new Font("Monospaced", Font.BOLD, 12);
-    public final static Font blockFontSmall_Plain = new Font("Monospaced", Font.PLAIN, 7);
-    public final static Font blockFontMedium_Plain = new Font("Monospaced", Font.PLAIN, 10);
-    public final static Font blockFontLarge_Plain = new Font("Monospaced", Font.PLAIN, 12);
+    public final static Font blockFontSmall_Bold = new Font("Monospaced", Font.BOLD, 9);
+    public final static Font blockFontMedium_Bold = new Font("Monospaced", Font.BOLD, 12);
+    public final static Font blockFontLarge_Bold = new Font("Monospaced", Font.BOLD, 14);
+    public final static Font blockFontSmall_Plain = new Font("Monospaced", Font.PLAIN, 9);
+    public final static Font blockFontMedium_Plain = new Font("Monospaced", Font.PLAIN, 12);
+    public final static Font blockFontLarge_Plain = new Font("Monospaced", Font.PLAIN, 14);
     private LabelWidget widget;
 
     /** These keys inputs are delegated back to renderable block */
